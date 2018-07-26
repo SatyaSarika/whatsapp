9e49973fb5cee4d8a73ef33d23e3926ab56d7104
SatyaSarika <sunkara.satyasarika@gmail.com>
2018-07-26 21:28:13 +0530

    commit

--- whatsupp/settings.py
+++ whatsupp/settings.py
@@ -81,15 +81,27 @@ WSGI_APPLICATION = 'whatsupp.wsgi.application'
 #     }
 # }
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'wtsupdb',
-        'HOST': 'localhost',
-        'USER':"root",
-        # 'PASSWORD': "root"
-        'PASSWORD': "Sarika@06"
+if 'RDS_HOSTNAME' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
     }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'wtsupdb',
+            'HOST': 'localhost',
+            'USER':"root",
+            # 'PASSWORD': "root"
+            'PASSWORD': "Sarika@06"
+        }
 }
 
 
