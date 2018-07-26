dea3ec8b79f420e12b99f008a3ff78f67d6f82cb
SatyaSarika <sunkara.satyasarika@gmail.com>
2018-07-26 21:48:43 +0530

    commit 21:48:42.98,26-07-2018

--- push.bat
+++ push.bat
@@ -1,5 +1,5 @@
 git init
 git add .
 git commit -m "commit %time%,%date%"
-git remote add -m  origin whatsapp https://github.com/SatyaSarika/whatsapp.git
+git remote add -m   whatsapp https://github.com/SatyaSarika/whatsapp.git
 git push -u origin master
